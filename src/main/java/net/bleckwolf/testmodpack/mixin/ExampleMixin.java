e1675e3e66c5bcab33a003ca5aa5be194bb36185
BleckWolf25 <joao.coutinho08@icloud.com>
2024-12-25 12:37:19 +0000

    ExampleMixin.java changes

--- src/main/java/net/bleckwolf/testmodpack/mixin/ExampleMixin.java
+++ src/main/java/net/bleckwolf/testmodpack/mixin/ExampleMixin.java
@@ -1,11 +1,14 @@
+// Packages
 package net.bleckwolf.testmodpack.mixin;
 
+// Imports
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+// Main Class
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
